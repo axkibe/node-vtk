02d3fc60ace3e1df03ab25f897c77062819d777d
Axel Kittenberger <axkibe@gmail.com>
2016-01-20 14:26:08 +0100

    a

---
+++ examples/cone.js
@@ -0,0 +1,31 @@
+// makies a cone
+'use strict';
+var vtk, source, mapper, actor, renderer, renderWindow, renderWindowInteractor;
+
+vtk = require('./vtk');
+
+source = new vtk.coneSource( );
+source.setResolution( 10 );
+
+mapper = new vtk.polyDataMapper( );
+
+mapper.setInputConnection(0, source.getOutputPort( 0 ) );
+
+actor = new vtk.actor( );
+actor.setMapper( mapper );
+
+renderer = new vtk.renderer( );
+renderer.addActor( actor );
+renderer.setBackground( .3, .2, .1 );
+renderer.resetCamera( );
+
+renderWindow = new vtk.renderWindow( );
+renderWindow.addRenderer( renderer );
+
+renderWindowInteractor = new vtk.renderWindowInteractor( );
+renderWindowInteractor.setRenderWindow( renderWindow );
+renderWindowInteractor.setInteractorStyle( new vtk.interactorStyleTrackballCamera( ) );
+
+renderWindow.render();
+
+renderWindowInteractor.start( );
